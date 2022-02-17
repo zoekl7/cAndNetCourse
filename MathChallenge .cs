8125ef9ab3cbe52e0c0a484a450ce7d1009110d0
Zoe <zoeoklein@gmail.com>
2022-02-16 18:07:42 -0800

    working on

---
+++ MathChallenge .cs
@@ -0,0 +1,4 @@
+int num1 = 5;
+int num2 = 7;
+int total = num1 + num2;
+Console.writeLine(total);
---
+++ math_challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "test";
+            string num2 = "testing";
+            string total = num1 + num2;
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
